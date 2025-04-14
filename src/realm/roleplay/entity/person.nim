4d82bb9a6ab8dd9865a06d1c0afbc4cace063bf7
Grabli66 <grabli66@gmail.com>
2025-04-14 20:04:46 +0300

    Первый коммит

---
+++ src/realm/roleplay/entity/person.nim
@@ -0,0 +1,87 @@
+import strformat
+
+import ai_connector/common/text
+
+# Тип для видов действий персонажа
+type PersonActionKind* = enum
+    # Действие, которое требует ответа от игрока
+    talk
+    # Действие за исключением разговора, передвижения, сна
+    action
+    # Действие передвижения
+    move
+    # Действие сна
+    sleep
+
+# Тип для действий персонажа
+type PersonAction* = object
+    case kind: PersonActionKind    
+    of PersonActionKind.talk:
+        talk: string    
+    of PersonActionKind.action:
+        action: string
+    of PersonActionKind.move:
+        move: string
+    of PersonActionKind.sleep:
+        sleep: string
+
+# Тип персонажа
+type Person* = object
+    # Является ли персонаж главным
+    isMain*: bool
+    # Имя персонажа
+    name*: string
+    # Пол персонажа
+    sex*: string
+    # Возраст персонажа
+    age*: int
+    # Внешность персонажа
+    look*: Text
+    # Характеристики персонажа
+    character*: Text
+    # Мотивация персонажа: инстинкты, желания, цели
+    motivation*: Text
+    # Память персонажа
+    memory*: Text
+
+# Персонаж с действиями
+type PersonWithActions* = object
+    # Персонаж
+    person*: Person
+    # Действия персонажа
+    actions*: seq[PersonAction]
+
+# Создает нового персонажа
+proc newPerson*(
+        isMain:bool,
+        name:string, 
+        sex:string,
+        age:int, 
+        look:Text,
+        character:Text, 
+        motivation:Text, 
+        memory:Text):Person =
+    result = Person(
+        isMain: isMain,
+        name: name, 
+        sex: sex,
+        age: age, 
+        look: look,
+        character: character, 
+        motivation: motivation, 
+        memory: memory)
+
+# Оператор для вывода персонажа в виде строки
+proc `$`*(person: Person): string =
+    var txt = newText()
+    txt.add(fmt"{person.name}")
+    txt.add(fmt"Пол: {person.sex} Возраст: {person.age}")
+    txt.add(fmt"Внешность: {person.look}")
+    txt.add(fmt"Характер: {person.character}")
+    txt.add(fmt"Мотивация: {person.motivation}")
+    txt.add(fmt"Память: {person.memory}")
+    return txt.toString()
+
+# Создает нового персонажа с действиями
+proc newPersonWithActions*(person: Person, actions: seq[PersonAction]): PersonWithActions =
+    result = PersonWithActions(person: person, actions: actions)
---
+++ src/realm/roleplay/writer_expert.nim
@@ -0,0 +1,114 @@
+# Писатель который придумывает истории
+
+import options
+import json
+import strformat
+import ai_connector/openai_api
+import ai_connector/common/text
+import entity/person
+
+type
+    # Модель для генерации текста
+    WriterExpert* = object
+        # API
+        api: OpenAiApi
+        # Модель для генерации текста
+        model: string
+
+# Конструктор писателя который использует заданную модель
+proc newWriterExpert*(api: OpenAiApi, model: string): WriterExpert =        
+    return WriterExpert(
+        api: api,
+        model: model
+    )
+
+# Генерация персонажа с нуля
+proc generatePerson*(writer: WriterExpert): Person =
+    # Пользовательский промт
+    var userPrompt = newText()
+
+    let result = writer.api.complete(writer.model, @[], userPrompt, CompleteOptions(
+        temperature: some(0.7),
+        max_tokens: some(1000),
+        stream: some(false),
+        structuredResponse: some(
+            %* {
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "Имя персонажа",
+                        "minLength": 1
+                    },
+                    "surname": {
+                        "type": "string",
+                        "description": "Фамилия персонажа",
+                        "minLength": 1
+                    },
+                    "age": {
+                        "type": "integer",
+                        "description": "Возраст персонажа",
+                        "minimum": 0
+                    },
+                    "sex": {
+                        "type": "string",
+                        "description": "Пол персонажа",
+                        "enum": [
+                            "Мужской",
+                            "Женский",
+                            "Другое"
+                        ]
+                    },
+                    "look": {
+                        "type": "string",
+                        "description": "Описание внешности персонажа",
+                        "minLength": 1
+                    },
+                    "character": {
+                        "type": "string",
+                        "description": "Описание характера персонажа",
+                        "minLength": 1
+                    },
+                    "motivation": {
+                        "type": "string",
+                        "description": "Мотивация персонажа",
+                        "minLength": 1
+                    },
+                    "memory": {
+                        "type": "string",
+                        "description": "Предыстория персонажа",
+                        "minLength": 1
+                    }
+                },
+                "required": [
+                    "name",
+                    "surname",
+                    "age",
+                    "sex",
+                    "look",
+                    "character",
+                    "motivation",
+                    "memory"
+                ],
+                "additionalProperties": false
+            }
+        )
+    ))
+
+    let jsonResult = parseJson(result)    
+    let name = jsonResult["name"].getStr()
+    let surname = jsonResult["surname"].getStr()
+    let age = jsonResult["age"].getInt()
+    let sex = jsonResult["sex"].getStr()
+    let look = jsonResult["look"].getStr()
+    let character = jsonResult["character"].getStr()
+    return Person(
+        name: fmt"{name} {surname}",
+        age: age,
+        sex: sex,
+        look: newText(look),
+        character: newText(character),
+        motivation: newText(""),
+        memory: newText("")
+    )
